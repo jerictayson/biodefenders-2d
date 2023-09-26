a83d52d3b91afb9ac8e743efaabbc10fcd8382e1
Jeric Tayson <jeric_tayson@protonmail.com>
2023-09-26 12:05:40 +0800

    Add: Ground checking for the player

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,55 +1,78 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+namespace Player
 {
-    private float _horizontal;
-    private Rigidbody2D _body;
-    private Animator _animator;
-    [SerializeField]
-    private float _moveSpeed = 5f;
-    // Start is called before the first frame update
-    void Start()
+    public class PlayerMovement : MonoBehaviour
     {
-        _body = GetComponent<Rigidbody2D>();
-        _animator = GetComponent<Animator>();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        Walk();
-        if (Input.GetKeyDown(KeyCode.Space))
+        private float _horizontal;
+        private Rigidbody2D _body;
+        private Animator _animator;
+        private BoxCollider2D _collider;
+        [SerializeField]
+        private float _moveSpeed = 5f;
+        [SerializeField]
+        private LayerMask _groundLayer;
+        // Start is called before the first frame update
+        void Start()
         {
-            Jump();
+            _body = GetComponent<Rigidbody2D>();
+            _animator = GetComponent<Animator>();
+            _collider = GetComponent<BoxCollider2D>();
         }
+
+        // Update is called once per frame
+        void Update()
+        {
+            Walk();
+            if (Input.GetKeyDown(KeyCode.Space) && _isGrounded())
+            {
+                Jump();
+            }
         
-        UpdateSprite();
-    }
-
-    private void Walk()
-    {
-        _horizontal = Input.GetAxis("Horizontal");
-        _body.velocity = new Vector2(_horizontal * _moveSpeed, _body.velocity.y);
-    }
-
-    private void UpdateSprite()
-    {
-        if(_horizontal > 0)
-        {
-            transform.localScale = new Vector3(1, 1, 1);
+            UpdateSprite();
         }
-        else if(_horizontal < 0)
+
+        private void Walk()
         {
-            transform.localScale = new Vector3(-1, 1, 1);
+            _horizontal = Input.GetAxis("Horizontal");
+            _body.velocity = new Vector2(_horizontal * _moveSpeed, _body.velocity.y);
         }
+
+        private void UpdateSprite()
+        {
+            if(_horizontal > 0)
+            {
+                transform.localScale = new Vector3(1, 1, 1);
+            }
+            else if(_horizontal < 0)
+            {
+                transform.localScale = new Vector3(-1, 1, 1);
+            }
         
-        _animator.SetBool("running", _horizontal != 0);
-    }
+            _animator.SetBool("running", _horizontal != 0);
+        }
 
-    private void Jump()
-    {
-        _body.velocity = new Vector2(_body.velocity.x, 5f);
+        private void Jump()
+        {
+            _body.velocity = new Vector2(_body.velocity.x, 5f);
+        }
+
+        private void OnCollisionEnter2D(Collision2D other)
+        {
+            Debug.Log(other.gameObject.name);
+        }
+
+        private bool _isGrounded()
+        {
+            Debug.Log("Checking if grounded");
+            var bounds = _collider.bounds;
+            return Physics2D.BoxCast(bounds.center, bounds.size, 0f, 
+                Vector2.down, .1f, _groundLayer);
+        }
+
+        public bool CanAttack()
+        {
+            return _horizontal == 0;
+        }
     }
 }
